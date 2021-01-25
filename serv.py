797cf87aa185a2bccdb0b7e6b257407d1182ec0c
Bootcamp Windows <xingruichen2@gmail.com>
2021-01-25 15:40:40 -0800

    Push

--- serv.py
+++ serv.py
@@ -1,7 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
  
- 
+ # comment just to test things out
 class StaticServer(BaseHTTPRequestHandler):
  
     def do_GET(self):
